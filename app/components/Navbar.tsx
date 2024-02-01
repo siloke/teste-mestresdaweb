10b077728eb6ecdf27002fd4177504dbcdb33fc3
Lucas <71577106+siloke@users.noreply.github.com>
2024-01-31 23:46:26 -0300

    add background

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import styled from "styled-components";
+
+const NavbarStyled = styled.div`
+    width: 100vw;
+    height: 114px;
+    background-color: #000;
+    box-shadow: 0px 4px 4px var(--red);
+`
+
+const Navbar = () => {
+    return (
+        <NavbarStyled>
+
+        </NavbarStyled>
+    );
+}
+
+export default Navbar;
