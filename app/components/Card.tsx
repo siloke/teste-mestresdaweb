55b8b5bafd3444bf5b8bd8e6ed9b34af42d77a5f
Lucas Vidal <71577106+siloke@user.noreply.github.com>
2024-02-02 08:05:08 -0300

    arruma card

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,21 +2,31 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import YellowStar from "./YellowStar";
+import CloseIcon from "./CloseIcon";
 
 const CardContainer = styled.div`
+    background: linear-gradient(to left, rgb(255, 0, 0), rgba(128, 0, 0));
     display: flex;
+    width: fit-content;
+    border-radius: var(--border-radius);
     margin: 50px;
 `
-
 const CardExpand = styled(motion.div)`
-    background: linear-gradient(to left, rgb(255, 0, 0), rgba(128, 0, 0));
-    position: absolute;
-    z-index: -1;
-    width: 659px;
+    width: 333px;
     height: 439px;
     border-radius: var(--border-radius);
+    color: var(--white);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
 `
-
+const ExpandWrapper= styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`
 const CardImage = styled.div`
     background-image: url('/heros/wanda-maximoff.jpg');
     background-size: cover;
@@ -32,28 +42,75 @@ const ImageDescription = styled(motion.div)`
     height: 50%;
     border-radius: var(--border-radius);
     transform: translateY(100%);
+    color: var(--white);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1.4rem;
 `
-
-// const variants: { open: { opacity: number }, closed: { opacity: number } } = {
-//     open: { opacity: 1 },
-//     closed: { opacity: 0 },
-// }
+const ImageTitle = styled.h2`
+    font-size: 20px;
+`
+const ImageText = styled.p`
+    font-size: 12px;
+    line-height: 20px;
+    text-align: justify;
+`
+const ExpandTitle = styled.h2`
+    font-size: 30px;
+`
+const ExpandText = styled.p`
+    font-size: 18px;
+    line-height: 22px;
+`
+const DetailsText = styled.p`
+    font-size: 20px;
+    cursor: pointer;
+`
 
 const Card = (): JSX.Element => {
     const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
+    const toggleExpand = () : void => {
+        setIsExpanded(!isExpanded);
+    }
+
     return (
-        <CardContainer onClick={() => setIsExpanded(!isExpanded)}>
+        <CardContainer> 
             <CardImage>
                 {!isExpanded &&
                     <ImageDescription >
+                        <ImageTitle>Wanda Maximoff</ImageTitle>
+                        <ImageText>Wanda Maximoff foi sequestrada da Sérvia e trazida para a Montanha Wundagore, base do Alto Evolucionário. Durante anos, ela e seu irmão gêmeo, Pietro, acreditavam que eram filhos de um casal de ciganos.</ImageText>
+                        <DetailsText onClick={toggleExpand}>ver detalhes</DetailsText>
                     </ImageDescription>
                 }
             </CardImage>
-            {
-                isExpanded &&
+            {isExpanded &&
                 <CardExpand animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ ease: "linear", delay: 0.3 }}>
-
+                    <ExpandWrapper >
+                        <ExpandTitle>Wanda Maximoff</ExpandTitle>
+                        <ExpandText>
+                            Aparições: <br />
+                            Vingadores - Era de Ultron <br />
+                            Capitão América - Guerra Civil <br />
+                            Vingadores - Guerra Infinita <br />
+                            Vingadores - Ultimato <br />
+                            WandaVision <br />
+                        </ExpandText>
+                        <ExpandTitle>Avaliações dos Fãs</ExpandTitle>
+                        <div>
+                            <YellowStar />
+                            <YellowStar />
+                            <YellowStar />
+                            <YellowStar />
+                            <YellowStar />
+                        </div>
+                    </ExpandWrapper>
+                    <div onClick={toggleExpand}>
+                        <CloseIcon />
+                    </div>
                 </CardExpand>
             }
         </CardContainer >
